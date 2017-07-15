a98b42b5ad3bea6d1f0b9477da4272085a918b48
johnwatkins0 <johnwatkins0@gmail.com>
2017-07-15 15:52:22 -0400

    Create repo

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# react-editable-element
-Allows HTML elements to be modified in place by changing them into inputs when clicked. 
+# react-editable-html-element
+Allows HTML elements to be modified in place by changing them into inputs when clicked.
+
+## Install
+
+```
+npm install --save react-editable-html-elements
+```
+
+## Usage
+
+```javascript
+import React from 'react';
+import EditableElement from 'react-editable-html-element';
+
+const YourComponent = () => {
+  const yourProps = {
+    // See Prop section
+  };
+
+  return <EditableElement {...yourProps} />;
+}
+
+```
+
+## Props
+
+| Name | Type | Description | Default |
+|------|------|-------------|---------|
+| allowEditing | bool | Set to false to disable editing completely. | true |
+| editable | bool | Should the element be editable when mounted? | false; true if the value prop is empty |
+| element | string (see src/htmlTags.js for the allowed HTML elements) | The HTML element. | 'div' |
+| elementClassName | string | CSS class(es) to apply to the element. | '' |
+| elementStyle | object | React-formatted CSS to apply to the element. | {} |
+| inputClassName | string | CSS classes(es) to apply to the input. | '' |
+| inputStyle | object | React-formatted CSS to apply to the input. | {} |
+| onChange | function | Override the component's built-in input change. | null |
+| onKeyDown | function | Override the component's built-in onKeyDown function. | null |
+| placeholder | string | Text to display when the input is empty. | 'Enter text' |
+| value | string | The value of the element/input. | '' |
---
+++ src/index.js
@@ -0,0 +1,146 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import htmlTags from './htmlTags';
+
+class EditableElement extends React.Component {
+  static propTypes = {
+    allowEditing: PropTypes.bool,
+    editable: PropTypes.bool,
+    element: (props, propName, componentName) => {
+      if (htmlTags.includes(props[propName])) {
+        return null;
+      }
+
+      return new Error(
+        `Invalid prop \`${propName}\` supplied to` +
+          ` \`${componentName}\`. Allowed elements: ${htmlTags.join(', ')}`
+      );
+    },
+    elementClassName: PropTypes.string,
+    elementStyle: PropTypes.objectOf(PropTypes.any),
+    inputClassName: PropTypes.string,
+    inputStyle: PropTypes.objectOf(PropTypes.any),
+    onChange: PropTypes.func,
+    onKeyDown: PropTypes.func,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+  };
+
+  static defaultProps = {
+    allowEditing: true,
+    allowedElements: htmlTags,
+    editable: false,
+    element: 'div',
+    elementClassName: '',
+    elementStyle: {},
+    inputClassName: '',
+    inputStyle: {},
+    onChange: null,
+    onKeyDown: null,
+    placeholder: 'Enter text',
+    value: '',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.value,
+      editable: this.props.allowEditing === true
+        ? this.props.value === '' || this.props.editable
+        : false,
+    };
+
+    this.renderStaticElement = this.renderStaticElement.bind(this);
+    this.renderEditableElement = this.renderEditableElement.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.state.editable) {
+      this.input.focus();
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.editable === false && this.props.editable === true) {
+      this.input.focus();
+      return;
+    }
+
+    if (prevState.editable === false && this.state.editable === true) {
+      this.input.focus();
+    }
+  }
+
+  onChange(value) {
+    if (this.props.onChange) {
+      this.props.onChange(value);
+      return;
+    }
+
+    this.setState({ value });
+  }
+
+  onClick() {
+    if (this.props.allowEditing === true) {
+      this.setState({ editable: true });
+    }
+  }
+
+  onKeyDown(event) {
+    if (this.props.onKeyDown) {
+      this.props.onKeyDown(event);
+      return;
+    }
+
+    const key = event.keyCode || event.which;
+
+    if ([13, 27].includes(key)) {
+      this.setState({ editable: false });
+    }
+  }
+
+  renderStaticElement() {
+    const Element = this.props.element;
+
+    return (
+      <Element
+        className={this.props.elementClassName}
+        style={this.props.elementStyle}
+        onClick={this.onClick}
+        dangerouslySetInnerHTML={{
+          __html: this.state.value || '(empty element)',
+        }}
+      />
+    );
+  }
+
+  renderEditableElement() {
+    return (
+      <input
+        ref={(input) => {
+          this.input = input;
+        }}
+        className={this.props.inputClassName}
+        style={this.props.inputStyle}
+        value={this.state.value}
+        placeholder={this.props.placeholder}
+        onChange={(event) => this.onChange(event.target.value)}
+        onKeyDown={this.onKeyDown}
+      />
+    );
+  }
+
+  render() {
+    if (this.state.editable) {
+      return this.renderEditableElement();
+    }
+    return this.renderStaticElement();
+  }
+}
+
+export default EditableElement;
