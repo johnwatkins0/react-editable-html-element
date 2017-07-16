144836d6184431b7344c78ff29b0642e2d8e4a67
johnwatkins0 <johnwatkins0@gmail.com>
2017-07-16 12:05:12 -0400

    Import README into docs

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,50 +2,141 @@ import React from 'react';
 import { render } from 'react-dom';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { tomorrowNightEighties } from 'react-syntax-highlighter/dist/styles';
+import ReactMarkdown from 'react-remarkable';
+import smoothScroll from 'smooth-scroll';
 
 import examples from './examples';
+
+class Site extends React.Component {
+  static renderExamples() {
+    return (
+      <article className="articleExamples">
+        <section>
+          <header>
+            <h4>Description</h4>
+          </header>
+          <div>Rendered element</div>
+          <div>Code</div>
+        </section>
+        {examples.map(example =>
+          (<section key={example.description}>
+            <header>
+              <h4>
+                {example.description}
+              </h4>
+            </header>
+            <div>
+              {example.element}
+            </div>
+            <div>
+              <SyntaxHighlighter
+                language="javascript"
+                style={tomorrowNightEighties}
+                showLineNumbers
+              >
+                {example.code}
+              </SyntaxHighlighter>
+            </div>
+          </section>),
+        )}
+      </article>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: window.location.hash === '#examples' ? 'examples' : 'docs',
+      docsSource: '',
+    };
+
+    this.renderHeader = this.renderHeader.bind(this);
+    this.renderBody = this.renderBody.bind(this);
+    this.renderDocs = this.renderDocs.bind(this);
+  }
+
+  componentWillMount() {
+    fetch(
+      'https://raw.githubusercontent.com/johnwatkins0/react-editable-html-element/master/README.md',
+    )
+      .then(response => response.text())
+      .then((docsSource) => {
+        this.setState({ docsSource });
+      });
+  }
+
+  componentDidMount() {
+    window.scroll(0, 0);
+  }
+
+  renderHeader() {
+    return (
+      <header className="page-header">
+        <h5>react-editable-html-element</h5>
+        <div>
+          <button
+            onClick={() => {
+              this.setState({ page: 'docs' }, () => {
+                smoothScroll.animateScroll(document.body);
+              });
+            }}
+          >
+            Docs
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ page: 'examples' }, () => {
+                smoothScroll.animateScroll(document.body);
+              });
+            }}
+          >
+            Examples
+          </button>
+        </div>
+        <a href="https://github.com/johnwatkins0/react-editable-html-element">GitHub</a>
+      </header>
+    );
+  }
+
+  renderDocs() {
+    if (this.state.docsSource.length) {
+      return (
+        <article className="articleDocs">
+          <ReactMarkdown source={this.state.docsSource} />
+        </article>
+      );
+    }
+
+    return null;
+  }
+
+  renderBody() {
+    if (this.state.page === 'examples') {
+      return Site.renderExamples();
+    }
+
+    return this.renderDocs();
+  }
+
+  render() {
+    const header = this.renderHeader();
+    const body = this.renderBody();
+
+    return (
+      <div>
+        {header}
+        {body}
+      </div>
+    );
+  }
+}
 
 const initElements = () => {
   Array.prototype.forEach.call(
     document.querySelectorAll('[data-editable-element-examples]'),
     (container) => {
-      render(
-        <div>
-          <header className="page-header">
-            <h5>react-editable-html-element - Examples</h5>
-            <a href="https://github.com/johnwatkins0/react-editable-html-element">GitHub</a>
-          </header>
-          <section>
-            <header>
-              <h4>Description</h4>
-            </header>
-            <div>Rendered element</div>
-            <div>Code</div>
-          </section>
-          {examples.map(example =>
-            (<section key={example.description}>
-              <header>
-                <h4>
-                  {example.description}
-                </h4>
-              </header>
-              <div>
-                {example.element}
-              </div>
-              <div>
-                <SyntaxHighlighter
-                  language="javascript"
-                  style={tomorrowNightEighties}
-                  showLineNumbers
-                >
-                  {example.code}
-                </SyntaxHighlighter>
-              </div>
-            </section>),
-          )}
-        </div>,
-        container,
-      );
+      render(<Site />, container);
     },
   );
 };
